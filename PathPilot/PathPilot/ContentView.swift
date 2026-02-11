632cbb390b471b1fd7bf9b39e01d90256e5ba43c
Marius <671440@stud.hvl.no>
2026-02-11 09:58:42 +0100

    Initial iOS project setup

---
+++ PathPilot/PathPilot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PathPilot
+//
+//  Created by Marius Horn on 11/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Sondrey!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PathPilot/PathPilot/PathPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathPilotApp.swift
+//  PathPilot
+//
+//  Created by Marius Horn on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PathPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
